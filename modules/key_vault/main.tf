1d54b40a93adb8a0a17a4945465091ed418b48ca
Ramon Barbosa <ramon@rbarbosa.me>
2026-02-14 19:30:08 +0000

    Add role assignment for Key Vault Secrets Officer to manage secrets in CI/CD

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -23,10 +23,21 @@ resource "azurerm_key_vault" "this" {
 }
 
 # =============================================================================
+# Key Vault Secrets Officer Role for CI/CD SP (required to manage secrets)
+# =============================================================================
+resource "azurerm_role_assignment" "deployer_key_vault_secrets_officer" {
+  scope                = azurerm_key_vault.this.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+# =============================================================================
 # Key Vault Secrets
 # =============================================================================
 resource "azurerm_key_vault_secret" "firebase_credential_json" {
   name         = "User--Firebase--CredentialJson"
   value        = var.firebase_credential_json
   key_vault_id = azurerm_key_vault.this.id
+
+  depends_on = [azurerm_role_assignment.deployer_key_vault_secrets_officer]
 }
